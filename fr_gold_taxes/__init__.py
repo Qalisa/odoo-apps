06150eb1c88a0126774265d880595ce21b56d010
Guillaume VARA <guillaume.vara@gmail.com>
2025-03-12 14:19:02 +0100

    ok

--- fr_gold_taxes/__init__.py
+++ fr_gold_taxes/__init__.py
@@ -3,7 +3,7 @@ from .models.fr_gold_taxes import GoldTaxCreator
 
 # Hooks pour l'initialisation
 def create_gold_tax_groups(env):    
-    # Utilisation de savepoint pour assurer l'atomicité
+    """Hook pour l'initialisation des taxes lors de l'installation du module."""
     with env.cr.savepoint():
         creator = GoldTaxCreator(env)
         creator.create_all_tax_groups()
