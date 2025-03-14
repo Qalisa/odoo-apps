5c1b59394f3f88942b181a2b24a7ce64de1433c9
Guillaume VARA <guillaume.vara@gmail.com>
2025-03-14 14:30:13 +0100

    add new module (#1)
    * ok
    
    * ok
    
    * ok
    
    * zz
    
    * zzz
    
    * kkk
    
    * ok
    
    * ok !

---
+++ contacts_additional_info/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Contacts - Informations Supplémentaires',
+    'version': '1.0',
+    'summary': 'Ajoute des informations supplémentaires aux contacts',
+    'description': """
+Champs Supplémentaires pour Contacts
+====================================
+Ce module ajoute les champs suivants aux contacts:
+- Date de Naissance
+- Lieu de Naissance
+- Numéro CNI (Carte Nationale d'Identité)
+
+Ces informations sont également affichées sur les avoirs, factures et commandes.
+    """,
+    'category': 'Contacts',
+    'author': 'Qalisa',
+    'website': 'https://www.qalisa.fr',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/report_views.xml',
+        'views/views.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
