356df61869624c63c202c97cd5c78d5e5d73ffac
Guillaume VARA <guillaume.vara@gmail.com>
2025-03-14 12:43:13 +0100

    zzz

--- contacts_additional_info/models/res_partner.py
+++ contacts_additional_info/models/res_partner.py
@@ -8,19 +8,3 @@ class ResPartner(models.Model):
     birthdate = fields.Date(string='Date de Naissance')
     birthplace = fields.Char(string='Lieu de Naissance')
     national_id_card_number = fields.Char(string='Numéro de Carte Nationale d\'Identité', help="Numéro de la Carte Nationale d'Identité")
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-    
-    # Champs calculés pour afficher les informations du partenaire
-    partner_birthdate = fields.Date(related='partner_id.birthdate', string='Date de Naissance', readonly=True)
-    partner_birthplace = fields.Char(related='partner_id.birthplace', string='Lieu de Naissance', readonly=True)
-    partner_national_id_card_number = fields.Char(related='partner_id.national_id_card_number', string='N° CNI', readonly=True)
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-    
-    # Champs calculés pour afficher les informations du partenaire
-    partner_birthdate = fields.Date(related='partner_id.birthdate', string='Date de Naissance', readonly=True)
-    partner_birthplace = fields.Char(related='partner_id.birthplace', string='Lieu de Naissance', readonly=True)
-    partner_national_id_card_number = fields.Char(related='partner_id.national_id_card_number', string='N° CNI', readonly=True)
