311960c65e5d8e9abe5e7821ff63e38e3450d49a
Guillaume VARA <guillaume.vara@gmail.com>
2025-03-15 13:04:49 +0100

    okay !

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,13 +2,22 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+//
+const site = (() => {
+	let site = process.env.CANONICAL_URL // must be process.env and not import.meta.env
+	if (site == null || site == "") return "http://localhost:4321"
+	if (site.startsWith('http://') || site.startsWith('https://')) return site
+	return 'https://' + site
+  })();
+
 // https://astro.build/config
 export default defineConfig({
+	site,
 	integrations: [
 		starlight({
 			title: 'My Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/Qalisa/odoo-apps',
 			},
 			sidebar: [
 				{
