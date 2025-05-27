2f680615cb1133c28966f041c81b24e9b98c0221
Guillaume VARA <guillaume.vara@gmail.com>
2025-05-27 09:15:45 +0200

    zzzz

--- addons/contacts_additional_info/__manifest__.py
+++ addons/contacts_additional_info/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Contacts - Informations Supplémentaires',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'summary': 'Ajoute des informations supplémentaires aux contacts (Date et lieu de naissance, No CNI)',
     'description': """
 Champs Supplémentaires pour Contacts
