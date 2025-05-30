496342bf7b3b12938d01b44d43edf76c823e0e86
Guillaume VARA <guillaume.vara@gmail.com>
2025-05-30 11:48:28 +0200

    replace CNI by broader ID proof; also, rename the addon

--- addons/contacts_citizenship_id/__manifest__.py
+++ addons/contacts_citizenship_id/__manifest__.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Contacts - Informations Supplémentaires',
-    'version': '1.0.4',
-    'summary': 'Ajoute des informations supplémentaires aux contacts (Date et lieu de naissance, No CNI)',
+    'name': "Contacts - Identification civile",
+    'version': '1.1.0',
+    'summary': """
+Ajoute des informations supplémentaires aux contacts (Date et lieu de naissance, Justificatif d'identité),
+et les affiche si présents sur les documents générés par Odoo.
+""",
     'description': """
 Champs Supplémentaires pour Contacts
 ====================================
 Ce module ajoute les champs suivants aux contacts:
 - Date de Naissance
 - Lieu de Naissance
-- Numéro CNI (Carte Nationale d'Identité)
+- Justificatif d'identité (nature, et numéro associé)
 
 Ces informations sont également affichées sur les avoirs, factures et commandes.
-    """,
+""",
     'category': 'Contacts',
     'author': 'Qalisa',
     'website': 'https://www.qalisa.fr',
