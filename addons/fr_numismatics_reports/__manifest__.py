fbe76c7ba41a015cb41df6c0a416f7ad4ca30795
Guillaume VARA <guillaume.vara@gmail.com>
2025-06-19 12:50:46 +0200

    fr_numismatics_reports: bump 1.1.2 (adds file title on report download)

--- addons/fr_numismatics_reports/__manifest__.py
+++ addons/fr_numismatics_reports/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Qalisa",
     'website': "https://odoo-docs.qalisa.fr/",
     'license': "AGPL-3",
-    'version': "1.1.1",
+    'version': "1.1.2",
     'depends': ['account'],
     'data': [
         'security/ir.model.access.csv',
