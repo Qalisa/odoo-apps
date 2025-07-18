d7caf1b7d830ebbee1fcd63f799f808a9635e7fe
Guillaume VARA <guillaume.vara@gmail.com>
2025-07-18 12:40:31 +0200

    add `contacts_title_in_name`

---
+++ addons/contacts_title_in_name/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Contacts - Nom avec Gentilé",
+    "summary": "Si configuré, affiche le gentilé (sinon appelé `Titre`) d'un contact dans le champ de destination des documents générés le concernant.",
+    "version": "18.0.1.0.0",
+    "depends": ["base"],
+    "author": "Qalisa",
+    "category": "Customization",
+    "data": [
+        "views/contact_name_template.xml",
+    ],
+    "installable": True,
+}
